21bf2bb41197db20841c33ac2c464ed098b27569
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-09-22 17:16:13 +0200

    Add task to compute percentage of https-urls in cleaned dataset.

--- distributed_task_queue_system/src/data_processing/subscribers/subscriber.py
+++ distributed_task_queue_system/src/data_processing/subscribers/subscriber.py
@@ -1,21 +1,22 @@
 import redis
-from src.data_processing.tasks import average_computation
+from src.data_processing.tasks import average_computation, https_url_percentage
 
 
 def subscriber():
     redis_client = redis.Redis(host='localhost', port=6379, db=0)
     pubsub = redis_client.pubsub()
-    pubsub.subscribe('cleaned_data_created', 'average_computation_done')
+    pubsub.subscribe('cleaned_data_created', 'computation_done')
     count_average_computations = 0
     for message in pubsub.listen():
         if message['type'] == 'message':
             data = message['data'].decode('utf-8')  # Get the data/payload of the message
             channel = message['channel'].decode('utf-8')  # Get the channel/intent of the message
 
-            if channel == "average_computation_done":  # Break the loop when both computations are done
+            if channel == "computation_done":  # Break the loop when both computations are done
                 count_average_computations += 1
-                if count_average_computations == 2:
+                if count_average_computations == 3:
                     break
             elif channel == "cleaned_data_created":  # When the cleaned data is available start the data processing
                 average_computation.delay(data, "num_comments", "Ask HN")
                 average_computation.delay(data, "num_comments", "Show HN")
+                https_url_percentage.delay(data)
--- distributed_task_queue_system/src/data_processing/tasks.py
+++ distributed_task_queue_system/src/data_processing/tasks.py
@@ -17,5 +17,14 @@ def average_computation(
     average_comments = df[target_column_head].mean()
     result_key = f"average:{selection_type}:{target_column_head}"
     store_average_value(average_comments, result_key)
-    publish_event("average_computation_done", result_key)
+    publish_event("computation_done", result_key)
     return average_comments
+
+
+@app.task
+def https_url_percentage(file_path: str) -> float:
+    df = pd.read_csv(file_path)
+    df_https = df[df['url'].str.contains("https")]
+    percentage_https = (df_https.shape[0] / df.shape[0]) * 100
+    publish_event("computation_done", "")
+    return percentage_https
