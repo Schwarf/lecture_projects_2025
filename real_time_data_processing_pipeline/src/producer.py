680953b132306c795d81ddb9b00ffdc5f96b9125
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-09-23 13:59:54 +0200

    Extend very basic example.

--- real_time_data_processing_pipeline/src/producer.py
+++ real_time_data_processing_pipeline/src/producer.py
@@ -1,11 +1,21 @@
+import time
+
 from kafka import KafkaProducer
+from src.simulated_temperature_sensor.temperature_sensor import simulate_temperature_with_outliers
 import json
 
-# Create a Kafka producer
-producer = KafkaProducer(bootstrap_servers='localhost:9092',
+
+def get_kafka_producer() -> KafkaProducer:
+    return KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-# Send a message
-producer.send('test', {'message': 'Hello Kafka'})
-producer.flush()
+
+def send_data_to_kafka(topic) -> None:
+    producer = get_kafka_producer()
+    temperature_sensor = simulate_temperature_with_outliers()
+    for reading in temperature_sensor:
+        message = {"temperature": reading, "timestamp": time.time()}
+        producer.send(topic, message)
+        producer.flush()
+        time.sleep(1)
 
--- real_time_data_processing_pipeline/src/simulated_temperature_sensor/temperature_sensor.py
+++ real_time_data_processing_pipeline/src/simulated_temperature_sensor/temperature_sensor.py
@@ -2,9 +2,16 @@ import time
 import random
 
 
-def temperature_sensor(base_temp=25.0, variance=2.0):
-    """ Simulate temperature readings with random fluctuation around a base temperature."""
+def simulate_temperature_with_outliers(base_temp=25.0, variance=2.0, outlier_chance=0.01):
+    """ Simulate temperature readings with random fluctuation and occasional outliers. """
     while True:
-        simulated_temp = base_temp + random.uniform(-variance, variance)
+        if random.random() < outlier_chance:
+            # Generate an outlier
+            outlier_multiplier = random.uniform(1, 5*variance)
+            outlier_variance = variance * outlier_multiplier
+            simulated_temp = base_temp + random.choice([-1, 1]) * random.uniform(variance, outlier_variance)
+        else:
+            # Normal fluctuation
+            simulated_temp = base_temp + random.uniform(-variance, variance)
         yield simulated_temp
-        time.sleep(1)  
+        time.sleep(1)
