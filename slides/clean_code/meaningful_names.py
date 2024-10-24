1e4db321f9ab14753102189502f96b8dd3af511c
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-10-24 10:00:22 +0200

    Start with simple code examples.

---
+++ slides/clean_code/meaningful_names.py
@@ -0,0 +1,38 @@
+# BAD: 'd' does not explain what the variable holds
+d = 86400
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# BETTER: A slightly better name, but it's still not very clear
+seconds_in_a_day = 86400
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# GOOD: The name makes the intent explicit
+SECONDS_PER_DAY = 86400
