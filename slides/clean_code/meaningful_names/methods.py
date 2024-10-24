c16cd84db706dff35456cbc388bf4f030abe3dc3
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-10-24 10:03:08 +0200

    WIP

---
+++ slides/clean_code/meaningful_names/methods.py
@@ -0,0 +1,13 @@
+# BAD: What does process() even mean?
+def process():
+    pass
+
+
+# BETTER: This name is better, but still vague.
+def handle_data():
+    pass
+
+
+# GOOD: Now it's clear what the function does
+def calculate_daily_revenue():
+    pass
