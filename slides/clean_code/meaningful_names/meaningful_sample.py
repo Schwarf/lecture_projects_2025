0fe0240653d6c793411e5ed56cb0fbc7b2e540d9
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-10-24 13:05:10 +0200

    Meaningful names applied.

---
+++ slides/clean_code/meaningful_names/meaningful_sample.py
@@ -0,0 +1,16 @@
+from math import sqrt
+
+
+def quadratic_equation_solver(quadratic_coefficient, linear_coefficient, constant):
+    if quadratic_coefficient == 0:
+        if linear_coefficient == 0:
+            return None
+        return -constant / linear_coefficient,
+
+    discriminant = linear_coefficient / quadratic_coefficient * linear_coefficient / quadratic_coefficient / 4 - constant / quadratic_coefficient
+    if discriminant < 0:
+        return None,
+    elif discriminant == 0:
+        return -linear_coefficient / quadratic_coefficient / 2,
+    else:
+        return -linear_coefficient / quadratic_coefficient / 2 + sqrt(discriminant), -linear_coefficient / quadratic_coefficient / 2 - sqrt(discriminant)
