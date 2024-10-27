744fd4966baabb1db4a0ae0a37238065f40969b8
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-10-27 10:03:31 +0100

    Refactor clean code sample using 'function-rules'

---
+++ slides/clean_code/functions/clean_code_functions_sample.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+def linear_equation_solver(linear_coefficient, constant):
+    if linear_coefficient == 0:
+        return None
+    return -constant / linear_coefficient,
+
+
+def quadratic_equation_solver(quadratic_coefficient, linear_coefficient, constant):
+    discriminant = linear_coefficient / quadratic_coefficient * linear_coefficient / quadratic_coefficient / 4 - constant / quadratic_coefficient
+    if discriminant < 0:
+        return None,
+    elif discriminant == 0:
+        return -linear_coefficient / quadratic_coefficient / 2,
+    else:
+        return -linear_coefficient / quadratic_coefficient / 2 + sqrt(discriminant), -linear_coefficient / quadratic_coefficient / 2 - sqrt(discriminant)
+
+
+def equation_solver(quadratic_coefficient, linear_coefficient, constant):
+    if quadratic_coefficient == 0:
+        return linear_equation_solver(linear_coefficient, constant)
+    else:
+        return quadratic_equation_solver(quadratic_coefficient, linear_coefficient, constant)
