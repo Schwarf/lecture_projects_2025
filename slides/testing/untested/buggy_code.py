86114e42621b1ab0dc586b7d9899cf28db4274c8
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-10-30 10:23:23 +0100

    Searching for good-buggy code.

--- slides/testing/untested/buggy_code.py
+++ slides/testing/untested/buggy_code.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from typing import List
 
 
+# Find the bug
 def calculate_total_price(prices: List[Decimal], discount_threshold: Decimal, discount_rate: Decimal) -> Decimal:
     total = sum(prices, Decimal(0))
     one  = Decimal(1)
