21fbf66a8da4b3d0b3a421fd68ca3756f8c9f530
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-11-09 16:04:43 +0100

    Using parametrized tests. Still needs improvement.

--- slides/testing/unit_tests/improved_test_linear_equation_solver.py
+++ slides/testing/unit_tests/improved_test_linear_equation_solver.py
@@ -1,11 +1,6 @@
 import pytest
-from typing import Union
-
-# Assuming the function is defined as:
-def linear_equation_solver(linear_coefficient: float, constant: float) -> Union[None, float]:
-    if linear_coefficient == 0:
-        return None
-    return -constant / linear_coefficient
+from math import sqrt, log, sin, cos, pi
+from testing.production_code import linear_equation_solver
 
 # Test for when the coefficient is zero
 @pytest.mark.parametrize(
@@ -28,9 +23,9 @@ def test_linear_equation_solver_zero_coefficient(linear_coefficient, constant, e
     [
         (2, -10, 5),
         (0.5, -1, 2),
-        (1, -3, 3),
+        (-1, 3, 3),
         (4, -20, 5),
-        (10, -50, 5)
+        (-10, 60, 6)
     ]
 )
 def test_linear_equation_solver_positive_solution(linear_coefficient, constant, expected_result):
@@ -44,8 +39,8 @@ def test_linear_equation_solver_positive_solution(linear_coefficient, constant, 
         (-4, 8, 2),
         (-2, 4, 2),
         (-0.5, 1, 2),
-        (-1, 3, -3),
-        (-10, 50, -5)
+        (-1, 3, 3.0),
+        (-10, 50, 5.0)
     ]
 )
 def test_linear_equation_solver_negative_solution(linear_coefficient, constant, expected_result):
@@ -87,10 +82,12 @@ def test_linear_equation_solver_non_integer_solution(linear_coefficient, constan
     "linear_coefficient, constant, expected_result",
     [
         (1.2, -6.6, pytest.approx(5.5)),
-        (-2.8, 8.4, pytest.approx(-3)),
-        (3.3, -6.6, pytest.approx(2)),
-        (10.5, -21, pytest.approx(2)),
-        (0.75, -1.5, pytest.approx(2))
+        (-2.8, 8.4, pytest.approx(3.0)),
+        (3.3, -6.6, pytest.approx(2.0)),
+        (10.5, -21, pytest.approx(2.0)),
+        (0.75, -1.5, pytest.approx(2.0)),
+        (sqrt(0.75), sin(-0.51), pytest.approx(sin(0.51)/sqrt(0.75))),
+        (log(7509210.13131), cos(0.8888), pytest.approx(-cos(0.8888)/log(7509210.13131)))
     ]
 )
 def test_linear_equation_solver_float_inputs(linear_coefficient, constant, expected_result):
